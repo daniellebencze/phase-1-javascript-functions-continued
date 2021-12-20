a647d1999b7c484ea29da91bd7fd9f18997cdea3
Danielle Bencze <daniellebencze@live.com>
2021-12-19 22:07:25 -0800

    commit tree 3beafa9a11116674b69e7f6207fc6c8a3cfcc123

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code your solution here
+// // code your solution here
+// function saturdayFun (activity = "roller-skate") {
+//     return `This Saturday, I want to ${activity}!`;
+// };
+
+// function mondayWork (activity = "go to the office") {
+//     return `This Monday, I will ${activity}.`;
+// };
+
+// const wrapAdjective = function(symbol = "*") {
+//     return function (adjective = "a dedicated programmer") {
+//         return `You are ${symbol}${adjective}${symbol}!`
+//     } ;
+// }
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(location = "go to the office") {
+  return `This Monday, I will ${location}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(emphasis = "*") {
+  const innerFunction = function (compliment = "a hard worker") {
+    return `You are ${emphasis}${compliment}${emphasis}!`;
+  };
+  return innerFunction;
+}
+
+wrapAdjective("a dedicated programmer");
